22b0aa4501215fbf4bf8107b02cb9f5db6aa0bf6
samwanyua <samexample8@gmail.com>
2024-03-07 23:32:30 +0300

    Working with Numbers

---
+++ java_basics/src/Numbers.java
@@ -0,0 +1,17 @@
+public class Numbers {
+    public static void main(String [] args){
+        //Numbers
+        System.out.println(2 * 4);
+        System.out.println(3 % 4); //3
+
+        int num = 23;
+        num += 100;
+
+        //Math methods
+        System.out.println(Math.pow(2,5));
+        System.out.println(Math.sqrt(139));
+        System.out.println(Math.round(32.23423));
+
+        System.out.println(num);
+    }
+}
