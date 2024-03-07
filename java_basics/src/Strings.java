efc125de7208c5565bb2ef1a2b42d3ff8600a42b
samwanyua <samexample8@gmail.com>
2024-03-07 22:17:27 +0300

    Working with string

---
+++ java_basics/src/Strings.java
@@ -0,0 +1,13 @@
+public class Strings {
+    //strings
+    public static void main(String [] args){
+        String greeting = "Hello";
+
+        System.out.println(greeting.length());
+        System.out.println(greeting.charAt(0));
+        System.out.println(greeting.indexOf("o"));
+        System.out.println(greeting.indexOf("w"));
+        System.out.println(greeting.substring(3));
+        System.out.println(greeting.substring(1,3));
+    }
+}
