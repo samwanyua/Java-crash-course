491d94725e523902229b082264faf6c7ce7f8bdb
samwanyua <samexample8@gmail.com>
2024-03-07 18:31:40 +0300

    Implemented Printing in Java

---
+++ java_basics/src/Print.java
@@ -0,0 +1,6 @@
+public class Print {
+    public static void main(String [] args){
+        System.out.println("This will print and add a new line after");
+        System.out.print("This will not print a new line after");
+    }
+}
