c44d0f73891527536668261f398a05a2c814a055
samwanyua <samexample8@gmail.com>
2024-03-08 00:02:40 +0300

    Implemented methods

---
+++ java_basics/src/Methods.java
@@ -0,0 +1,11 @@
+public class Methods {
+    public static void main(String [] args){
+        // Methods
+        int sum = addNumbers(2342342, 2341);
+        System.out.println(sum);
+    }
+
+    public static int addNumbers(int num1, int num2){
+        return num1 + num2;
+    }
+}
