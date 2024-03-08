aad5ddf7cab854139952b5b4c434907b342a6862
samwanyua <samexample8@gmail.com>
2024-03-09 01:35:52 +0300

    Implemented while loops

---
+++ java_basics/src/While.java
@@ -0,0 +1,10 @@
+public class While {
+    public static void main(String [] args){
+        // While loops
+        int index = 1;
+        while(index <=5){
+            System.out.println("This is a while loop" + index);
+            index ++;
+        }
+    }
+}
