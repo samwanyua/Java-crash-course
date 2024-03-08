738991b851f4a35624e17fc04ccf7be7653a5f84
samwanyua <samexample8@gmail.com>
2024-03-09 01:32:28 +0300

    Implemented do while loops

---
+++ java_basics/src/Do_while.java
@@ -0,0 +1,10 @@
+public class Do_while {
+    public static void main(String [] args){
+        // Do while loops
+        int i = 0;
+        do{
+            System.out.println(i + " This is a do while loop");
+            i++;
+        }while(i <= 10);
+    }
+}
