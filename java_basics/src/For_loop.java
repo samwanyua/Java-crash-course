6c5331c44602d1ebcbfe430cf5c7c1f5f7c3511a
samwanyua <samexample8@gmail.com>
2024-03-09 05:51:06 +0300

    Implemented for loops

--- java_basics/src/For_loop.java
+++ java_basics/src/For_loop.java
@@ -4,5 +4,10 @@ public class For_loop {
         for(int i = 0; i <= 10; i++){
             System.out.println(i + " This is a for loop");
         }
+
+        int luckyNums [] = {4,8,15,16,23,42};
+        for(int luckyNum : luckyNums){
+            System.out.println(luckyNum);
+        }
     }
 }
